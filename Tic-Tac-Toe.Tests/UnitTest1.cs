1e894daec70c2b43dfd1823c57c1027f899d671a
GorginZ <georgia.z.leng@gmail.com>
2020-08-26 15:34:43 +1000

    going to add Player class, test for setname function

--- Tic-Tac-Toe.Tests/UnitTest1.cs
+++ Tic-Tac-Toe.Tests/UnitTest1.cs
@@ -19,5 +19,31 @@ namespace Tic_Tac_Toe.Tests
            Assert.Equal(expectedBoard, $"{aBoard[0][0]}{aBoard[0][1]}{aBoard[0][2]}\n{aBoard[1][0]}{aBoard[1][1]}{aBoard[1][2]}\n{aBoard[2][0]}{aBoard[2][1]}{aBoard[2][2]}"); 
           
         }
+
+        [Fact]
+        public void SetName()
+        { //arrange
+          var testPlayer = new Player();
+          //act
+          var testPlayer.SetName("Name");
+          //assert
+          var expectedName = "Name";
+                            
+           Assert.Equal(expectedName, testPlayer.Name); 
+          
+        }  
+//  [Fact]
+//         public void ValidateName()
+//         { //arrange
+//           var testPlayer = new Player();
+//           //act
+//           var testPlayer.Validate(null);
+//           //assert
+//           var expectedName = "Name";
+                            
+//            Assert.Equal(expectedName, testPlayer.Name); 
+          
+//         }  
+
     }
 }
